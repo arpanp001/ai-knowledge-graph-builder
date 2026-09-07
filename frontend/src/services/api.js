f514fa38a878bd38b3d729dc81f7bc686be5ddf5
Arpan Pawar <168514234+arpanp001@users.noreply.github.com>
2026-09-07 16:06:58 +0530

    frontend .env changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,10 +2,9 @@ import axios from "axios";
 import { getToken } from "./authService";
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000",
+    baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000",
 });
 
-// Automatically attach the JWT (if we have one) to every outgoing request
 api.interceptors.request.use((config) => {
     const token = getToken();
     if (token) {
